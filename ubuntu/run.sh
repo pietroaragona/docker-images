2416be2f584a21fa3dc8cc646c0e023a9e55fe83
Pietro Aragona <pietro.aragona@alliance-healtcare.net>
2016-07-21 22:21:22 +0200

    add volume for .gitconfig

--- ubuntu/run.sh
+++ ubuntu/run.sh
@@ -1,6 +1,7 @@
 docker run -it \
 -v  /etc/hosts:/etc/hosts  \
 -v  /home/pietro/repos:/home/pietro/repos \
+-v  /home/pietro/.gitconfig:/home/pietro/.gitconfig \
 --name myubuntu \
 --rm \
-myubuntu
+pietroaragona/myubuntu
