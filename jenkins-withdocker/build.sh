b18fec302a0e93aa33c813f840869a6345e3fa30
Pietro Aragona <pietro.aragona@vtsolutions.it>
2022-09-26 17:24:46 +0200

    jenkins new version

--- jenkins-withdocker/build.sh
+++ jenkins-withdocker/build.sh
@@ -1,7 +1,9 @@
 
 IMAGE_NAME="jenkins-withdocker"
+VERSION="2.361.1-lts-jdk11"
+FULL_TAG=${IMAGE_NAME}:${VERSION}
 
-docker build -t pietroaragona/$IMAGE_NAME .
+docker build -t pietroaragona/${FULL_TAG} .
 
-echo "\n\nTo push on docker Hub: \n\tdocker push pietroaragona/$IMAGE_NAME"
-#docker push pietroaragona/$IMAGE_NAME
+echo -e "\n\nTo push on docker Hub: \n\tdocker push pietroaragona/${FULL_TAG}"
+#docker push pietroaragona/$${FULL_TAG}
--- jenkins/build.sh
+++ jenkins/build.sh
@@ -2,8 +2,10 @@
 set -e
 
 IMAGE_NAME="jenkins"
+VERSION="2.361.1-lts-jdk11"
+FULL_TAG=${IMAGE_NAME}:${VERSION}
 
-docker build -t pietroaragona/$IMAGE_NAME .
+docker build -t pietroaragona/${FULL_TAG} .
 
-echo "\n\nTo push on docker Hub: \n\tdocker push pietroaragona/$IMAGE_NAME"
-#docker push pietroaragona/$IMAGE_NAME
+echo -e "\n\nTo push on docker Hub: \n\tdocker push pietroaragona/${FULL_TAG}"
+#docker push pietroaragona/${FULL_TAG}
