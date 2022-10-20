3d69d81ea26b72106043fb2b9e43bad0ff91ef21
Pietro <pietro.aragona@vtsolutions.it>
2022-10-20 22:32:15 +0200

    refactory

--- jenkins-withdocker/build.sh
+++ jenkins-withdocker/build.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -e
 
 IMAGE_NAME="jenkins-withdocker"
 VERSION="2.361.1-lts-jdk11"
@@ -6,4 +8,4 @@ FULL_TAG=${IMAGE_NAME}:${VERSION}
 docker build -t pietroaragona/${FULL_TAG} .
 
 echo -e "\n\nTo push on docker Hub: \n\tdocker push pietroaragona/${FULL_TAG}"
-#docker push pietroaragona/$${FULL_TAG}
+#docker push pietroaragona/${FULL_TAG}
